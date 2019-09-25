07cc04a5d7bb0c073d837e45e7b178cc72a00f98
Eric Zhang <yuanliang_zh@163.com>
2019-09-25 23:18:28 +0800

    LeetCode0011:  Container With Most Water

---
+++ src/leet0011/Solution0011.java
@@ -0,0 +1,30 @@
+package leet0011;
+
+import java.util.Scanner;
+
+public class Solution0011 {
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r])*(r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxarea;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String[] split = s.split(" ");
+        int[] height = new int[split.length];
+        for (int i = 0; i < height.length; i++) {
+            height[i] = Integer.parseInt(split[i]);
+        }
+
+        System.out.println(new Solution0011().maxArea(height));
+    }
+}
