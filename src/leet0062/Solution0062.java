f3678d71f8724cc2387fe3d505d9e758d9cd65f3
Eric Zhang <yuanliang_zh@163.com>
2019-09-26 12:05:14 +0800

    LeetCode0062:  Unique Paths

---
+++ src/leet0062/Solution0062.java
@@ -0,0 +1,36 @@
+package leet0062;
+
+import java.util.Scanner;
+
+public class Solution0062 {
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) {
+            return 0;
+        }
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        //定义dp[i][j]表示到达位置（i，j）的总路径数
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        //dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = Integer.parseInt(in.nextLine().trim());
+        int n = Integer.parseInt(in.nextLine().trim());
+        System.out.println(new Solution0062().uniquePaths(m, n));
+    }
+}
