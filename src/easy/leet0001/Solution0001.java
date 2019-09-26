2c06d397daccb182c71fb519edc2b35a13502688
Eric Zhang <yuanliang_zh@163.com>
2019-09-26 21:25:12 +0800

    LeetCode1: Two Sum(with HashMap)

--- src/easy/leet0001/Solution0001.java
+++ src/easy/leet0001/Solution0001.java
@@ -8,8 +8,8 @@ public class Solution0001 {
     /**
      * 两个数相加，得到目标和
      *
-     * @param nums
-     * @param target
+     * @param nums 加数数组
+     * @param target 目标和
      * @return 两数的索引
      */
     public int[] twoSum(int[] nums, int target) {
@@ -17,7 +17,8 @@ public class Solution0001 {
         //使用map记录(key--数组中的数值，val--数组中对应数值的索引)
         for (int i = 0; i < nums.length; i++) {
             int tmp = target - nums[i];
-            if (map.containsKey(tmp)) {     //当要求的加数出现在map中，读取其val即可得到索引，加上当前索引组成两个加数
+            //当要求的加数出现在map中，读取其val即可得到索引，加上当前索引组成两个加数
+            if (map.containsKey(tmp)) {
                 return new int[]{map.get(tmp), i};
             }
             map.put(nums[i], i);
