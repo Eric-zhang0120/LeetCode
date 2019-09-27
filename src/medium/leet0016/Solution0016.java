74a900ad68951620597605ae53b41a55eb132e3f
Eric Zhang <yuanliang_zh@163.com>
2019-09-27 15:42:52 +0800

    LeetCode16: 3Sum Closest

--- src/medium/leet0016/Solution0016.java
+++ src/medium/leet0016/Solution0016.java
@@ -7,26 +7,40 @@ public class Solution0016 {
         if (nums == null || nums.length < 3) {
             return Integer.MIN_VALUE;
         }
-        int res =  0;
+        int res =  Integer.MAX_VALUE;
         int len = nums.length;
         Arrays.sort(nums);
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < len; i++) {
             int l = i+1, r = len - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                int tmp = target - sum;
-                if (Math.abs(tmp) < min) {
-                    min = tmp;
-                    res = sum;
+            int rangeMin = nums[i] + nums[l] + nums[l+1];
+            int rangeMax = nums[i] + nums[r] + nums[r-1];
+            if (target < rangeMin) {
+                if (rangeMin - target < min) {
+                    min = rangeMin - target;
+                    res = rangeMin;
                 }
+            } else if (target > rangeMax) {
+                if (target - rangeMax < min) {
+                    min = target - rangeMax;
+                    res = rangeMax;
+                }
+            } else {
+                while (l < r) {
+                    int sum = nums[i] + nums[l] + nums[r];
+                    int tmp = target - sum;
+                    if (Math.abs(tmp) < min) {
+                        min = tmp;
+                        res = sum;
+                    }
 
-                if (tmp == 0) {
-                    return sum;
-                } else if(tmp > 0){
-                    l++;
-                } else {
-                    r--;
+                    if (tmp == 0) {
+                        return sum;
+                    } else if (tmp > 0) {
+                        l++;
+                    } else {
+                        r--;
+                    }
                 }
             }
         }
