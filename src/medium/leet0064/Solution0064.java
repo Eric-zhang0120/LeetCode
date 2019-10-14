b77d5f9d977aa3304913c9d5b0d4086b9688ff4c
Eric Zhang <yuanliang_zh@163.com>
2019-10-14 23:27:28 +0800

    LeetCode64: Minimum Path Sum

--- src/medium/leet0064/Solution0064.java
+++ src/medium/leet0064/Solution0064.java
@@ -8,24 +8,24 @@ public class Solution0064 {
 
         int r = grid.length;
         int c = grid[0].length;
+        //dp[i][j]表示到达位置（i， j）的最小路径和
+        //因此,dp[i][j]  = min(dp[i-1][j], dp[i][j-1])+grid[i][j]
         int[][] dp = new int[r][c];
         dp[0][0] = grid[0][0];
 
+        //设置第一列，只能通过上一个位置(i-1, 0)到达
         for (int i = 1; i < r; i++) {
             dp[i][0] = dp[i-1][0] + grid[i][0];
         }
 
+        //设置第一行，只能通过左一个位置(0, j-1)到达
         for (int j = 1; j < c; j++) {
             dp[0][j] = dp[0][j-1] + grid[0][j];
         }
 
         for (int i = 1; i < r; i++) {
             for (int j = 1; j < c; j++) {
-                if (dp[i-1][j] < dp[i][j-1]) {
-                    dp[i][j] = dp[i-1][j] + grid[i][j];
-                } else {
-                    dp[i][j] = dp[i][j-1] + grid[i][j];
-                }
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
             }
         }
 
